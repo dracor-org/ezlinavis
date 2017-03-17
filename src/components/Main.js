c356e2e77185532354bec298917fa498be3d760b
Carsten Milling <cmil@hashtable.de>
2017-03-17 14:08:47 +0100

    remove yeoman cruft

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,13 +3,10 @@ require('styles/App.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
-
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
       </div>
     );
