82037f31991df76ec40bf465fbd101941f311d31
Carsten Milling <cmil@hashtable.de>
2018-11-07 13:21:18 +0100

    fix xo errors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className="App">
         <header className="App-header">
