043130f01f1118b869cf7597eafef288912f84a4
Carsten Milling <cmil@hashtable.de>
2018-11-07 19:14:03 +0100

    copy files created by `yarn create react-app ezlinavis`

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/Main';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-// Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
